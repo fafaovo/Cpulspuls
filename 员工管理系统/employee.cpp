0a61b1e80a53e2146cc8752a9aaf85c052e15a18
fafaovo <aa1206180803@sina.cn>
2021-04-12 20:06:39 +0800

    修改

---
+++ 员工管理系统/employee.cpp
@@ -0,0 +1,22 @@
+#include "employee.h"
+
+Employee::Employee(int id, string name, int did)
+{
+	this->m_id = id;
+	this->m_name = name;
+	this->m_DeptId = did;
+}
+//个人信息
+void Employee::showInfo()
+{
+	cout << "员工编号" << this->m_id << "\t";
+	cout << "员工名字" << this->m_name << "\t";
+	cout << "员工岗位:" << this->getDeptName() << "\t";
+	cout << "岗位职责:完成经历交给的任务" << endl;
+	
+}
+//获取职工岗位名称;
+string Employee::getDeptName()
+{
+	return string("员工");
+}
